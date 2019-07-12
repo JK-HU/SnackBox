8470faccfc6c6e79c404079c89175751492535ea
JK-HU <1262195190@qq.com>
2019-07-12 15:33:37 +0800

    这次主要修改数据统计表页面,主要是折线统计图的展示

--- js/statistics_xs.js
+++ js/statistics_xs.js
@@ -2,9 +2,27 @@
 
     // 查询日期
     $('.query_date').bind('click',function() {
-        $('.start_date_ipt').val(); //开始日期
-        $('.end_date_ipt').val(); //结束日期
-        ajax_queryDate();
+        let startTime = $('.start_date_ipt').val(); //开始日期时间戳
+        let endTime = $('.end_date_ipt').val() //结束日期时间戳
+
+        var begintime = new Date(Date.parse($('.start_date_ipt').val().replace(/-/g,   "/"))).getTime();  
+        var endtime = new Date(Date.parse($('.end_date_ipt').val().replace(/-/g,   "/"))).getTime();     
+        var nTime = endtime - begintime;  
+        var day = Math.floor(nTime/86400000); //相差天数
+
+       
+        if(startTime > endTime) {
+            alert('开始日期不能大于结束日期');
+            return false;
+        }
+       
+        if( day > 30 ){
+            alert('日期最大间隔30天');
+            return false;
+        }
+
+
+        // ajax_queryDate();
     });
     
     // 查询日期发送请求, 这里没查询一次,整个页面的数据都要改变
@@ -21,3 +39,4 @@
     
 
 })();
+
